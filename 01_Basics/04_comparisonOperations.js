9b48475b1bb90144c20c7c8b5382fa6954588e70
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-18 19:30:45 +0000

    comparison operations

---
+++ 01_Basics/04_comparisonOperations.js
@@ -0,0 +1,13 @@
+console.log(1>2)
+console.log("1">2)
+//Before comparing , make sure that datatype of teh given values 
+//are same .
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+//This happened because comparison converts null to a number, treating it
+//as 0.
+console.log(null < 0)
+
+// "===" checks the data type as well apart from values hence known as strict check.
