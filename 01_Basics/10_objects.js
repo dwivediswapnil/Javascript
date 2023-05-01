3dccdbb151e891b2099a973ead44f629d3ec9c31
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-05-01 06:48:26 +0000

    objects

---
+++ 01_Basics/10_objects.js
@@ -0,0 +1,19 @@
+//singleton: Object created from a constructor 
+//singleton cannot  be created from literals
+//Object creation via constructor -->singleton
+//Object literals 
+const jsUser = {
+    name : "Swapnil",
+    age:32,
+    location:"Lucknow",
+    email: "lp@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays :['Monday','Tuesday']
+} //Object created 
+
+////////////////////////////////
+console.log(jsUser["email"])
+console.log(jsUser.email)
+////////////////////////////
+//These both ways aren't correct
+
